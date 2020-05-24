5a941603100eea1e87d1fb618051e091a6ab05f7
Alex <a80nieves@gmail.com>
2020-05-24 12:36:51 -0400

    added dev/prod

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: 'google client ID for development',
+    googleClientSecret: 'google client secret key for development',
+    mongoURI: 'atlas mongo URI for development',
+    cookieKey: 'your cookie secret key'
+}
