5665966de4ee26ec35568611c908b3e9fb445bd7
Alex <a80nieves@gmail.com>
2020-05-29 09:08:06 -0400

    changed modal functionality

---
+++ client/build/precache-manifest.bc9119e3bbf31a15b940e16d0e080854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5682a6e76db0df227809f96796756f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a378a0b2a0177f40ce5",
+    "url": "/static/css/main.a12c2ea8.chunk.css"
+  },
+  {
+    "revision": "a051464bcecbe335b0c1",
+    "url": "/static/js/2.b5765f48.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b5765f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a378a0b2a0177f40ce5",
+    "url": "/static/js/main.30b8dd23.chunk.js"
+  },
+  {
+    "revision": "a551078c39d75c94aee2",
+    "url": "/static/js/runtime-main.4463dbb3.js"
+  },
+  {
+    "revision": "7e835bf54f84222ae4db7860e907ce01",
+    "url": "/static/media/loading.7e835bf5.gif"
+  }
+]);
